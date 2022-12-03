1001dc0bd22267ce974ee5c4d082ee1ddb18ad0d
xsocratx <xsocratx@gmail.com>
2022-12-03 21:21:03 +0100

    Styling

--- svelte-study/src/App.svelte
+++ svelte-study/src/App.svelte
@@ -3,6 +3,16 @@
   let src = "/tutorial/image.gif";
 </script>
 
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 1em;
+	}
+</style>
+
 <h1>Hello, {name}!</h1>
 
 <img {src} alt="Dancing man" />
+
+<p>Paragraph</p>
